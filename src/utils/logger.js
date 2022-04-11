5227c3b0f9d5c4ef430a4dbf733af8c195bdb99e
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2022-04-11 14:50:36 +0100

    add middleware and controller - file struct

--- src/utils/logger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var pino_1 = require("pino");
-var logger = (0, pino_1["default"])({
-    transport: {
-        target: 'pino-pretty'
-    }
-});
-exports["default"] = logger;
